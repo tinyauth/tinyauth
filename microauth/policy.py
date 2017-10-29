211ddebdab7bda13aad555f4d1257bf83f2e6316
John Carr <john.carr@unrouted.co.uk>
2017-10-29 17:53:09 +0000

    Policy conditions

--- microauth/policy.py
+++ microauth/policy.py
@@ -1,4 +1,13 @@
 import fnmatch
+import ipaddress
+
+
+def _match_condition_ipaddress(left, right):
+    return ipaddress.ip_address(left) in ipaddress.ip_network(right)
+
+
+def _match_condition_notipaddress(left, right):
+    return not _match_condition_ipaddress(left, right)
 
 
 def _get_list(dict, key):
@@ -28,13 +37,32 @@ def _match_resource(statement, resource):
     return False
 
 
-def allow(policy, action, resource):
+_condition_functions = {
+    'IpAddress': _match_condition_ipaddress,
+    'NotIpAddress': _match_condition_notipaddress,
+}
+
+
+def _match_condition(statement, context):
+    # "Condition": {"IpAddress": {"aws:SourceIp": "203.0.113.0/24"}}
+    for condition_check, conditions in statement.get('Condition', {}).items():
+        fn = _condition_functions[condition_check]
+        for condition, value in conditions.items():
+            if not fn(context[condition], value):
+                return False
+
+    return True
+
+
+def allow(policy, action, resource, context=None):
     retval = "Default"
 
     for statement in _get_list(policy, 'Statement'):
         if not _match_action(statement, action):
             continue
         if not _match_resource(statement, resource):
+            continue
+        if not _match_condition(statement, context or {}):
             continue
         if statement['Effect'] == 'Deny':
             return "Deny"
--- microauth/tests/test_policy.py
+++ microauth/tests/test_policy.py
@@ -38,3 +38,94 @@ class TestSimplePolicy(unittest.TestCase):
             'myservice:ListInstances',
             'arn::myservice:::instances/foo_bar_1',
         ) == "Deny"
+
+
+class TestPolicyConditions(unittest.TestCase):
+
+    def test_ip_address_allow(self):
+        policy = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'myservice:ListInstances',
+                'Resource': 'arn::myservice:::instances/foo_*',
+                'Condition': {
+                    'IpAddress': {'SourceIp': '127.0.0.0/24'}
+                },
+                'Effect': 'Allow',
+            }]
+        }
+
+        assert allow(
+            policy,
+            'myservice:ListInstances',
+            'arn::myservice:::instances/foo_1',
+            context={
+                'SourceIp': '127.0.0.1',
+            }
+        ) == "Allow"
+
+    def test_ip_address_deny(self):
+        policy = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'myservice:ListInstances',
+                'Resource': 'arn::myservice:::instances/foo_*',
+                'Condition': {
+                    'IpAddress': {'SourceIp': '127.0.0.0/24'}
+                },
+                'Effect': 'Allow',
+            }]
+        }
+
+        assert allow(
+            policy,
+            'myservice:ListInstances',
+            'arn::myservice:::instances/foo_1',
+            context={
+                'SourceIp': '192.168.0.1',
+            }
+        ) == "Default"
+
+    def test_not_ip_address_allow(self):
+        policy = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'myservice:ListInstances',
+                'Resource': 'arn::myservice:::instances/foo_*',
+                'Condition': {
+                    'NotIpAddress': {'SourceIp': '127.0.0.0/24'}
+                },
+                'Effect': 'Allow',
+            }]
+        }
+
+        assert allow(
+            policy,
+            'myservice:ListInstances',
+            'arn::myservice:::instances/foo_1',
+            context={
+                'SourceIp': '192.168.0.1',
+            }
+        ) == "Allow"
+
+    def test_not_ip_address_deny(self):
+        policy = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Action': 'myservice:ListInstances',
+                'Resource': 'arn::myservice:::instances/foo_*',
+                'Condition': {
+                    'NotIpAddress': {'SourceIp': '127.0.0.0/24'}
+                },
+                'Effect': 'Allow',
+            }]
+        }
+
+        assert allow(
+            policy,
+            'myservice:ListInstances',
+            'arn::myservice:::instances/foo_1',
+            context={
+                'SourceIp': '127.0.0.1',
+            }
+        ) == "Default"
