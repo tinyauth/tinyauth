928778b5f87d48be91ae2bcd88ea910fa781df3b
John Carr <john.carr@unrouted.co.uk>
2017-10-31 09:36:23 +0000

    Improve Docker packaging

--- docker/flask/entrypoint.sh
+++ docker/flask/entrypoint.sh
@@ -12,7 +12,11 @@ if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
 fi
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
+if [ -z "$POSTGRES_HOST" ]; then
+    export POSTGRES_HOST=postgres
+fi
+
+export DATABASE_URI=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_USER
 
 
 function postgres_ready(){
@@ -20,7 +24,7 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="$POSTGRES_HOST")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
