8c42d0cea6a3af30aa09fbc8b1986dcdb2c33704
John Carr <john.carr@unrouted.co.uk>
2017-11-07 16:25:48 +0000

    Example of calling authorise API with openresty

--- examples/httpbin/nginx/httpbin_auth.lua
+++ examples/httpbin/nginx/httpbin_auth.lua
@@ -1,6 +1,43 @@
-local remote_addr = ngx.var.remote_addr
-local uri = ngx.var.uri
+local cjson = require "cjson"
+local http = require "resty.http"
 
-if not remote_addr or not string.match(remote_addr, "172.25.0.1") then
+local headers = {}
+local i = 1
+for k, v in pairs(ngx.req.get_headers(raw)) do
+  headers[i] = {k, v}
+  i = i + 1
+end
+
+local body = {
+    action = "HttpReverseProxy",
+    resource = "urn:::" .. ngx.var.uri,
+    headers = headers,
+    context = {
+        SourceIP = ngx.var.remote_addr
+    }
+}
+
+ngx.log(ngx.ERR, cjson.encode(body))
+
+local httpc = http.new()
+local res, err = httpc:request_uri("http://microauth:5000/api/v1/authorize", {
+    method = "POST",
+    body = cjson.encode(body),
+    headers = {
+        ["Content-Type"] = "application/json",
+        Authorization = "Basic Z2F0ZWtlZXBlcjprZXltYXN0ZXI="
+    }
+})
+
+if not res then
+  ngx.log(ngx.ERR, err) 
   ngx.exit(ngx.HTTP_FORBIDDEN)
 end
+
+local auth = cjson.decode(res.body)
+ngx.log(ngx.ERR, res.body) 
+if not auth['Authorized'] then
+  ngx.exit(ngx.HTTP_FORBIDDEN)
+end
+
+ngx.req.set_header('X-User', 'gatekeeper')
