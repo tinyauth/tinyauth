091a15e392cec2526451746e96773f8eb2928f4d
John Carr <john.carr@unrouted.co.uk>
2017-12-05 17:14:32 +0000

    Batch authorize

--- tinyauth/resources/service.py
+++ tinyauth/resources/service.py
@@ -16,6 +16,12 @@ authorize_parser.add_argument('resource', type=str, location='json', required=Tr
 authorize_parser.add_argument('headers', type=list, location='json', required=True)
 authorize_parser.add_argument('context', type=dict, location='json', required=True)
 
+batch_authorize_parser = reqparse.RequestParser()
+batch_authorize_parser.add_argument('steps', type=list, location='json', required=True)
+batch_authorize_parser.add_argument('headers', type=list, location='json', required=True)
+batch_authorize_parser.add_argument('context', type=dict, location='json', required=True)
+
+
 
 @service_blueprint.route('/api/v1/authorize-login', methods=['POST'])
 def service_authorize_login():
@@ -45,5 +51,37 @@ def service_authorize():
         headers=Headers(args['headers']),
         context=args['context'],
     )
+
+    return jsonify(result)
+
+
+@service_blueprint.route('/api/v1/batch-authorize-by-token', methods=['POST'])
+def batch_service_authorize():
+    internal_authorize('BatchAuthorizeByToken', f'arn:tinyauth:')
+
+    args = batch_authorize_parser.parse_args()
+
+    result = {
+       'Steps': [],
+       'Authorized': True,
+       'Identity': 'guest',
+    }
+
+    # FIXME: Refactor this to only need to verify identity once and reuse the extracted policies for multiple verifications
+    for step in args['steps']:
+        step_result = external_authorize(
+            action=step['action'],
+            resource=step['resource'],
+            headers=Headers(args['headers']),
+            context=args['context'],
+        )
+
+        result['Steps'].append(step_result['Authorized'])
+
+        if not step_result['Authorized']:
+            result['Authorized'] = False
+
+    if result['Authorized']:
+        result['Identity'] = step_result['Identity']
 
     return jsonify(result)
