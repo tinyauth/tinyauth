6bcb5a8bf69f175d3fb0055a687b87370453177f
John Carr <john.carr@unrouted.co.uk>
2017-12-13 15:03:46 +0000

    Add gevent

--- docker/flask/gunicorn.sh
+++ docker/flask/gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/gunicorn --config /app/etc/gunicorn.conf --log-config /app/etc/logging.conf tinyauth.wsgi:app
+/usr/local/bin/gunicorn --worker-class gevent --config /app/etc/gunicorn.conf --log-config /app/etc/logging.conf tinyauth.wsgi:app
