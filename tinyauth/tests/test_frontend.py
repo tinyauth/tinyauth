7ecf74b993e714d6df086803b31ca3efc4cfd1ba
John Carr <john.carr@unrouted.co.uk>
2018-01-29 14:44:48 +0000

    Test non DEBUG variants of / and /login

--- tinyauth/tests/test_frontend.py
+++ tinyauth/tests/test_frontend.py
@@ -1,4 +1,5 @@
 import json
+from unittest import mock
 
 from . import base
 
@@ -20,18 +21,46 @@ class TestLoggedInUI(base.TestCase):
 
         assert {} == json.loads(response.get_data(as_text=True))
 
-    def test_index(self):
+    def test_index_DEBUG(self):
         self.app.config['DEBUG'] = True
         response = self.client.get('/')
         assert response.status_code == 200
         assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
-        assert response.get_data(as_text=True).strip().endswith('</html>')
+
+        body = response.get_data(as_text=True).strip()
+        assert '<script type="text/javascript" src="/static/js/bundle.js">' in body
+        assert body.endswith('</html>')
+
+    def test_index(self):
+        assets = json.dumps({
+            'main.js': 'static/js/bundle.c0ff33.js',
+            'main.css': 'static/css/main.c0ff33.css',
+        })
+
+        with mock.patch('tinyauth.frontend.open', mock.mock_open(read_data=assets)):
+            response = self.client.get('/')
+
+        assert response.status_code == 200
+        assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+
+        body = response.get_data(as_text=True).strip()
+        assert '<script type="text/javascript" src="/static/js/bundle.c0ff33.js">' in body
+        assert '<link href="/static/css/main.c0ff33.css" rel="stylesheet">' in body
+        assert body.endswith('</html>')
 
     def test_login(self):
-        self.app.config['DEBUG'] = True
         response = self.client.get('/login')
         assert response.status_code == 302
         assert response.headers['Location'] == 'http://localhost/'
+
+    def test_logout(self):
+        assert len(self.client.cookie_jar._cookies['localhost.local']['/']) == 2
+
+        response = self.client.get('/logout')
+        assert response.status_code == 302
+        assert response.headers['Location'] == 'http://localhost/login'
+
+        assert len(self.client.cookie_jar._cookies['localhost.local']['/']) == 0
 
     def test_use_api_with_token(self):
         response = self.client.get(
@@ -56,17 +85,33 @@ class TestLoggedInUI(base.TestCase):
 class TestLoggedOutUI(base.TestCase):
 
     def test_index(self):
-        self.app.config['DEBUG'] = True
-
         response = self.client.get('/')
 
         assert response.status_code == 302
         assert response.headers['Location'] == 'http://localhost/login'
 
-    def test_login(self):
+    def test_login_DEBUG(self):
         self.app.config['DEBUG'] = True
 
         response = self.client.get('/login')
         assert response.status_code == 200
         assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
-        assert response.get_data(as_text=True).strip().endswith('</html>')
+
+        body = response.get_data(as_text=True).strip()
+        assert '<script type="text/javascript" src="/login/static/js/bundle.js">' in body
+        assert body.endswith('</html>')
+
+    def test_login(self):
+        assets = json.dumps({
+            'main.js': 'static/js/bundle.c0ff33.js',
+        })
+
+        with mock.patch('tinyauth.frontend.open', mock.mock_open(read_data=assets)):
+            response = self.client.get('/login')
+
+        assert response.status_code == 200
+        assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+
+        body = response.get_data(as_text=True).strip()
+        assert '<script type="text/javascript" src="/login/static/js/bundle.c0ff33.js">' in body
+        assert body.endswith('</html>')
